5e6ccf0eb0b44204e37f1ee3a2fabeea4b39c3a1
Su-Yong <simssy2205@gmail.com>
2020-06-15 06:13:17 +0900

    add message

--- client.py
+++ client.py
@@ -1,40 +1,57 @@
-clients = []
+from util import Singleton
+
+
+class ClientManager(Singleton):
+    clients = []
+
+    def find(self, uuid):
+        for element in self.clients:
+            if element.uuid == uuid:
+                return element
+
+        return None
+
+    def find_users(self, uuid):
+        result = []
+
+        for element in self.clients:
+            if element.__class__.__name__ == 'UserClient':
+                if element.target_id == uuid:
+                    result.append(element)
+
+        return result
+
+    def delete(self, uuid):
+        for element in self.clients:
+            if element.uuid == uuid:
+                self.clients.remove(element)
+
+    def all(self):
+        return self.clients
 
 
 class Client:
     id = ''
     type = ''
+    uuid = ''
 
-    def __init__(self, client_id, client_type):
-        self.id = client_id  # base64
+    def __init__(self, client_id, client_type, uuid):
+        self.id = client_id  # socket session id
         self.type = client_type  # bridge, user
+        self.uuid = uuid  # base62 identifier
 
-        clients.append(self)
-
-    @staticmethod
-    def find(client_id):
-        for element in clients:
-            if element.id == client_id:
-                return element
-
-        return None
-
-    @staticmethod
-    def delete(client_id):
-        for element in clients:
-            if element.id == client_id:
-                clients.remove(element)
-
-    @staticmethod
-    def all():
-        return clients
+        ClientManager.instance().clients.append(self)
 
 
 class BridgeClient(Client):
-    def __init__(self, client_id):
-        super().__init__(client_id, 'bridge')
+    def __init__(self, client_id, uuid):
+        super().__init__(client_id, 'bridge', uuid)
 
 
 class UserClient(Client):
-    def __init__(self, client_id):
-        super().__init__(client_id, 'user')
+    target_id = ''
+
+    def __init__(self, client_id, uuid, target_id):
+        self.target_id = target_id
+
+        super().__init__(client_id, 'user', uuid)
--- index.py
+++ index.py
@@ -1,15 +1,14 @@
-import time
-
 from flask import Flask, request
 from flask_socketio import SocketIO
 
-from client import Client, BridgeClient, UserClient
+from client import Client, BridgeClient, UserClient, ClientManager
 
 HOST = '127.0.0.1'
 PORT = 3000
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
+
 io = SocketIO(app)
 
 
@@ -17,7 +16,7 @@ io = SocketIO(app)
 def index():
     result = ''
 
-    for c in Client.all():
+    for c in ClientManager.instance().all():
         result += f'id: {c.id}, type: {c.type}\n'
 
     return result
@@ -25,36 +24,58 @@ def index():
 
 @io.on('connect')
 def connect():
-    print('connect', request.sid)
+    pass
 
 
 @io.on('register-client')
 def connect(data):
     client_id = request.sid
     client_type = data['type']
+    uuid = data['uuid']
 
-    if type == 'bridge':
-        BridgeClient(client_id)
-    elif type == 'user':
-        UserClient(client_id)
+    check = ClientManager.instance().find(uuid)
+
+    if check is not None:
+        ClientManager.instance().delete(uuid)
+
+    if client_type == 'bridge':
+        BridgeClient(client_id, uuid)
+    elif client_type == 'user':
+        UserClient(client_id, uuid, data['target'])
     else:
-        Client(client_id, client_type)
+        Client(client_id, client_type, uuid)
+
+
+@io.on('ping')
+def ping():
+    print("pong!",  request.sid)
 
 
 @io.on('disconnect')
 def disconnect():
-    print('disconnect', request.sid)
+    pass
 
-    Client.delete(request.sid)
+
+@io.on('unregister-client')
+def disconnect(data):
+    client_type = data['type']
+    client_uuid = data['uuid']
+
+    if client_type == 'bridge':
+        pass
+    ClientManager.instance().delete(client_uuid)
+
+    print("disconnect")
 
 
 @io.on('message')
 def message(data):
-    Client.find(request.sid)
+    client = ClientManager.instance().find(data['uuid'])
+    users = ClientManager.instance().find_users(client.uuid)
 
-    room = data['room']
-    sender = data['sender']
-    text = data['text']
+    for user in users:
+        print(user.uuid)
+        io.emit('message', data, room=user.id)
 
 
 if __name__ == '__main__':
